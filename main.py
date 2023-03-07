ef34475514002599615f67d7e714bfcb316aef60
Guiiz94 <92375190+Guiiz94@users.noreply.github.com>
2023-03-07 12:18:07 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,56 @@
+# Exercice 1
+# temp=6.892
+# distance=19.7
+
+# vitesse=distance/temp
+# print("La vitesse est de",vitesse,"km/h")
+
+# Exercice 2
+# nom = input("Quel est votre nom ?")
+# nom = str(nom)
+# age = input("Quel est votre age ?")
+# age = int(age)
+
+# print("Bonjour",nom,"vous avez",age,"ans")
+
+# Exercice 3
+# number = input("Entrez un nombre :")
+# number = int(number)
+
+# if number >= 0:
+#     print("La racine carrée de",number,"est",number**0.5)
+# else:
+#     print("Le nombre est négatif")
+
+# Exercice 4
+# mot1 = input("Entrez le premier mot :")
+# mot2 = input("Entrez le second mot :")
+
+# if len(mot1) < len(mot2):
+#     print("Le mot",mot1,"est plus court que",mot2)
+# elif len(mot1) > len(mot2):
+#     print("Le mot",mot2,"est plus court que",mot1)
+# else:
+#     print("Les mots sont de même longueur")
+
+# Exercice 5
+# pression = input("Entrez la pression :")
+# pression = float(pression)
+# pSeuil = 2.3
+# pSeuil = float(pSeuil)
+# volume = input("Entrez le volume :")
+# volume = float(volume)
+# vSeuil = 4.41
+# vSeuil = float(vSeuil)
+
+
+# if pression > pSeuil and volume > vSeuil:
+#     print("Arrêt immédiat")
+# elif pression > pSeuil and volume <= vSeuil:
+#     print("Augmenter le volume")
+# elif pression <= pSeuil and volume > vSeuil:
+#     print("Diminuer le volume")
+# else:
+#     print("Tout va bien")
+
+
